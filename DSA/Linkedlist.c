2e612f4af70d2b5b176e448a83a7bf973ae19cea
puniiith25 <puniiith25@gmail.com>
2025-08-12 19:59:09 +0530

    dsa

--- DSA/Linkedlist.c
+++ DSA/Linkedlist.c
@@ -1,31 +1,99 @@
 #include <stdio.h>
-struct node {
-    int data ;
-    struct node * next;
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node* next;
 };
-
+void append(struct node **head_ref, int data)
+{
+    struct node* newNode = (struct node*)  malloc(sizeof(struct node));
+    newNode -> data = data;
+    newNode -> next = NULL;
     
-
-
-void append(struct node ** head_ref , int data){
-    struct  node *new_node = (struct node*)malloc(sizeof(struct node));
-    new_node ->data=data;
-    new_node ->next=NULL;
-    if(*head_ref==NULL){
-        *head_ref= new_node;
-
+    if(*head_ref == NULL){
+        *head_ref = newNode;
     }else{
-        struct node *temp = *head_ref;
-        while (temp->next!=NULL)
-        {
-           temp=temp->next;
+        struct node* temp = *head_ref;
+        
+        while(temp -> next != NULL){
+            temp = temp -> next;
         }
-        temp->next=new_node;   
+        temp -> next = newNode;
+    }
+}
+void prepend(struct node** head_ref, int data){
+    
+    struct node* newNode = (struct node*) malloc (sizeof(struct node));
+    newNode -> data = data;
+    newNode -> next = NULL;
+    
+    newNode -> next = *head_ref;
+    *head_ref = newNode;
+}
+void delFront(struct node** head_ref){
+    
+    if(*head_ref == NULL)
+    {
+        printf("List is empty");
+    }else 
+    {
+        struct node* temp = *head_ref;
+        *head_ref = temp->next;
+        free(temp);
+    }
+}
+void delLast(struct node** head_ref){
+    if(*head_ref == NULL){
+        printf("List is empty");
+    }else
+    {
+        struct node* temp = *head_ref;
+        if(temp->next == NULL){
+            *head_ref = NULL;  
+            free(temp);
+        }else{
+            while(temp -> next -> next != NULL){
+                temp = temp->next;
+            }
+            // struct node* temp1 = temp-> next;
+            // temp->next = NULL;
+            free(temp->next);
+            temp->next = NULL;
+        }
+        
+        
+    }
+}
+ 
+void printList(struct node **head_ref){
+    
+    if(*head_ref == NULL){
+        printf("List is empty");
+    }else{
+        struct node* temp = *head_ref;
+        
+        while(temp != NULL){
+            printf("%d =>", temp->data);
+            temp = temp-> next;
+        }
+        printf("NULL\n");
     }
 }
 int main()
 {
-   
+     struct node* head = NULL;
+     
+     append(&head, 340);
+     append(&head, 333);
+     append(&head, 999);
+     append(&head, 3990);
+     delLast(&head);
+    printList(&head);
+    prepend(&head, 30);
+    prepend(&head,90);
+    prepend(&head, 888);
+    delFront(&head);
+    printList(&head);
 
     return 0;
 }
