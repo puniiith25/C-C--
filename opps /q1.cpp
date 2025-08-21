87bf7aaaba79668050b2463a70ad5929a8df1e20
puniiith25 <puniiith25@gmail.com>
2025-08-21 21:28:02 +0530

    dsa

---
+++ opps /q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the N Value";
+    cin>>n;
+    int arr[4];
+    while(n>0){
+        int i = n%10;
+       
+        n=n/10;
+    }
+    for(int i =0;i<4;i++){
+       
+    }
+
+
+    return 0;
+}
