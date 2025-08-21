e061b4f56573da6a90e38d759929f17926bb7afa
puniiith25 <puniiith25@gmail.com>
2025-08-21 21:49:48 +0530

    oops

--- opps /q1.cpp
+++ opps /q1.cpp
@@ -1,19 +1,36 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n;
-    cout<<"Enter the N Value";
+    int n ,k,f=0;
+    cout<<"Enter the N Value\n";
     cin>>n;
+    int n1=n;
     int arr[4];
-    while(n>0){
-        int i = n%10;
-       
-        n=n/10;
-    }
-    for(int i =0;i<4;i++){
-       
-    }
+    int i=3;
 
+    while(n>0){
+        arr[i] = n%10;
+    
+        n=n/10;
+        i--;
+    }
+    cout<<"Enter which position number you want in this  "<<n1<<"=";
+    cin>>k;
+    if(k<4){
+
+         for(int i =0;i<4;i++){
+        if(k==i){
+             cout<<arr[i];
+             f=1;
+             break;
+        }
+            
+    }
+    }else{
+        cout<<"invalid position";
+    }
+   
+    
 
     return 0;
 }
